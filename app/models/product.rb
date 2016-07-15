4b6781ae1f380bf3a2bd19f89c12bacd75445bb7
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-07-14 19:53:16 -0500

    ready test for price > 0

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
   belongs_to :category
+  before_validation :price_mayor_cero
+
+  def price_mayor_cero
+    if price <= 0
+      errors.add(:price, "el precio debe ser mayor a 0")
+      return false
+    end
+  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @product = products(:one)
+  end
+  test "Pice must be mayor to 0" do
+    @product.price = 0
+    assert_not @product.valid?
+  end
 end
