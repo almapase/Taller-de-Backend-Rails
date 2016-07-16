c4e21d00eb278d0ac4286c1b1a6d2a9151bacdf8
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-07-15 19:02:22 -0500

    Crear un link para borrar una categoría desde el index de categorías

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,15 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    respond_to do |format|
+      format.html { redirect_to categories_url, notice: 'Client was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'categories/index'
+  resources :categories
+  # get 'categories/index'
   get 'products/index'
   post 'products/filter'
 
