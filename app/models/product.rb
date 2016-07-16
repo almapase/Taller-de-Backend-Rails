0cc23f1d3ef3f274aca4030b0516ba0b7849d501
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-07-15 19:30:41 -0500

    mejora validacion de precio mayor que cero

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
   belongs_to :category
-  before_validation :price_mayor_cero, :default_value_for_premium
+  before_validation :default_value_for_premium
   validates :category, presence: true
+  validates :price, numericality: {greater_than: 0}
   after_destroy :destroy_category_after_last_product
 
   scope :premiums, -> { where(premium: true)  }
@@ -9,14 +10,6 @@ class Product < ActiveRecord::Base
   scope :last_n, -> (n){ order(created_at: :desc).limit(n)  }
   scope :filter_by_name, ->(query) { where("name like ?", "%#{query}%")  }
   scope :filter_by_price, ->(query) { where(price: query)  }
-
-
-  def price_mayor_cero
-    if price.nil? || price <= 0
-      errors.add(:price, "el precio debe ser mayor a 0")
-      return false
-    end
-  end
 
   def default_value_for_premium
     if premium.nil?
